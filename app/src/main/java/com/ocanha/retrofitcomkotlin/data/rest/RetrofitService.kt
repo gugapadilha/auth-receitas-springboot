8067189cf2658d01f546f19265dc4c4f20dc53c2
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-01-28 12:17:50 -0300

    creating retrofit service to connect with api through device

--- app/src/main/java/com/ocanha/retrofitcomkotlin/data/rest/RetrofitService.kt
+++ app/src/main/java/com/ocanha/retrofitcomkotlin/data/rest/RetrofitService.kt
@@ -1,9 +1,21 @@
 package com.ocanha.retrofitcomkotlin.data.rest
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
 interface RetrofitService {
 
     companion object {
 
+        private val retrofitService : RetrofitService by lazy {
+            val retrofitService = Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080") //doing that getting IP directly
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            retrofitService.create(RetrofitService::class.java )
+        }
 
     }
 }
